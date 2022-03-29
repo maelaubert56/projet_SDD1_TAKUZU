55f49568b6cfa6c011d7fff80100f8438d52496a
mariuschev <marius.cheva@gmail.com>
2022-03-29 15:48:12 +0200

    ajout menu principal

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("COUCOU\n");
+    printf("-----------------BIENVENU SUR NOTRE TAKUZU ! ---------------\n");
+    printf("Veuillez choisir une option : \n");
+    printf("1 - R%csoudre une grille \n",130);
+    printf("2 - R%csoudre une grille automatiquement \n",130);
+    printf("3 - G%cn%crer une grille de Takuzu\n",130,130);
     return 0;
 }
